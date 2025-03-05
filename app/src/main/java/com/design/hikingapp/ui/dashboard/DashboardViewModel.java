d9cbaba840551b9de2fe17949f2da0c4c2b50c58
Jeremiah Durham <jerduremiahham@gmail.com>
2025-03-05 12:42:41 -0700

    initial commit

---
+++ app/src/main/java/com/design/hikingapp/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.design.hikingapp.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
