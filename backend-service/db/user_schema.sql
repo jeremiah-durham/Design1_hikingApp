c9bf4efffcab3d310c34e4ebe227b1d626443b6c
Jeremiah Durham <jeremiah_durham@mines.edu>
2025-04-19 18:12:45 -0600

    Add user db tables

--- backend-service/db/user_schema.sql
+++ backend-service/db/user_schema.sql
@@ -3,9 +3,25 @@ USE project;
 
 CREATE TABLE users (
     pk INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    id binary(16) NOT NULL,
+    uuid binary(16) NOT NULL,
+    fname VARCHAR(32) NOT NULL,
+    lname VARCHAR(32) NOT NULL,
+    eemail VARCHAR(255) NOT NULL,
     weight INT NOT NULL,
     height INT NOT NULL,
-    eemail VARCHAR(255) NOT NULL,
-    UNIQUE(id)
+    UNIQUE(uuid)
+);
+
+
+CREATE TABLE hike_log (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    create_ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user binary(16) NOT NULL,
+    trail_id INT NOT NULL,
+    active BOOL NOT NULL DEFAULT FALSE,
+    start_time DATETIME DEFAULT NULL,
+    expected_end_time DATETIME DEFAULT NULL,
+
+    FOREIGN KEY (user) REFERENCES users(uuid),
+    FOREIGN KEY (trail_id) REFERENCES trails(id)
 );
